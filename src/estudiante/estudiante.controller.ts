7555eb1391edee8b2f3d4dd30fcd4b464fb2cc57
Jedom11 <j.dominguezp@uniandes.edu.co>
2025-05-16 09:17:42 -0500

    intento bono

---
+++ src/estudiante/estudiante.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { EstudianteService } from './estudiante.service';
+import { ActividadService } from 'src/actividad/actividad.service';
+
+@Controller()
+export class EstudianteController {
+  constructor(private readonly estudianteService: EstudianteService,
+    private readonly actividadService: ActividadService
+  ) {}
+}
